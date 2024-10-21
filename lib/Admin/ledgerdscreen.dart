907c4960f7b9c812b24eb2bb3d04abcd12fb765e
Ayushi Thumar <165537687+AThumar@users.noreply.github.com>
2024-10-21 08:31:46 +0530

    commmit

--- lib/Admin/ledgerdscreen.dart
+++ lib/Admin/ledgerdscreen.dart
@@ -18,8 +18,8 @@ class LedgerScreen extends StatelessWidget {
             decoration: const BoxDecoration(
               gradient: RadialGradient(
                 colors: [
-                  Color.fromARGB(255, 110, 102, 188), // Darker purple
-                  Colors.white, // Light center
+                  Color.fromARGB(255, 110, 102, 188),
+                  Colors.white,
                 ],
                 radius: 2,
                 center: Alignment(2.8, -1.0),
@@ -53,11 +53,13 @@ class LedgerScreen extends StatelessWidget {
           Column(
             children: [
               SizedBox(
-                height: 100.0, // This height should be slightly less than the blue container's height
+                height:
+                    100.0, // This height should be slightly less than the blue container's height
               ),
               Expanded(
                 child: Container(
-                  margin: const EdgeInsets.only(top: 16.0), // Add top margin for space
+                  margin: const EdgeInsets.only(
+                      top: 16.0), // Add top margin for space
                   width: double.infinity, // Make it full width
                   decoration: BoxDecoration(
                     gradient: LinearGradient(
@@ -75,12 +77,17 @@ class LedgerScreen extends StatelessWidget {
                       topRight: Radius.circular(60),
                     ),
                   ),
-                  child: Padding( // Use Padding to create space above the ListView
-                    padding: const EdgeInsets.only(top: 16.0), // Adjust the top padding as needed
+                  child: Padding(
+                    // Use Padding to create space above the ListView
+                    padding: const EdgeInsets.only(
+                        top: 16.0), // Adjust the top padding as needed
                     child: StreamBuilder<QuerySnapshot>(
-                      stream: FirebaseFirestore.instance.collection('orders').snapshots(),
+                      stream: FirebaseFirestore.instance
+                          .collection('orders')
+                          .snapshots(),
                       builder: (context, snapshot) {
-                        if (snapshot.connectionState == ConnectionState.waiting) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
                           return Center(child: CircularProgressIndicator());
                         }
 
@@ -92,19 +99,24 @@ class LedgerScreen extends StatelessWidget {
 
                         // Use a Set to track unique full names
                         Set<String> uniqueNames = {};
-                        Map<String, List<Map<String, dynamic>>> groupedOrders = {};
+                        Map<String, List<Map<String, dynamic>>> groupedOrders =
+                            {};
 
                         // Loop through the orders to filter unique names and group orders by fullName
                         for (var order in orders) {
                           final fullName = order['fullName'];
-                          final orderData = order.data() as Map<String, dynamic>;
+                          final orderData =
+                              order.data() as Map<String, dynamic>;
 
                           // If the full name is not already in the set, add it to both the set and the unique orders list
                           if (!uniqueNames.contains(fullName)) {
                             uniqueNames.add(fullName);
-                            groupedOrders[fullName] = [orderData]; // Initialize a new list for this name
+                            groupedOrders[fullName] = [
+                              orderData
+                            ]; // Initialize a new list for this name
                           } else {
-                            groupedOrders[fullName]!.add(orderData); // Add to the existing list for this name
+                            groupedOrders[fullName]!.add(
+                                orderData); // Add to the existing list for this name
                           }
                         }
 
@@ -114,23 +126,31 @@ class LedgerScreen extends StatelessWidget {
                             final fullName = uniqueNames.elementAt(index);
 
                             return Container(
-                              margin: const EdgeInsets.symmetric(vertical: 15, horizontal: 16), // Adjust margin as needed
+                              margin: const EdgeInsets.symmetric(
+                                  vertical: 15,
+                                  horizontal: 16), // Adjust margin as needed
                               decoration: BoxDecoration(
                                 color: Colors.white, // White background
-                                borderRadius: BorderRadius.circular(10), // Rounded corners
+                                borderRadius: BorderRadius.circular(
+                                    10), // Rounded corners
                                 boxShadow: [
                                   BoxShadow(
-                                    color: Colors.grey.withOpacity(0.5), // Shadow color
+                                    color: Colors.grey
+                                        .withOpacity(0.5), // Shadow color
                                     spreadRadius: 2, // Spread radius
                                     blurRadius: 5, // Blur radius
-                                    offset: const Offset(0, 3), // Changes position of shadow
+                                    offset: const Offset(
+                                        0, 3), // Changes position of shadow
                                   ),
                                 ],
                               ),
                               child: ListTile(
-                                title: Text(
-                                  'Name: $fullName', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold) // Dark text color
-                                ),
+                                title: Text('Name: $fullName',
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight:
+                                            FontWeight.bold) // Dark text color
+                                    ),
                                 onTap: () {
                                   // Navigate to LedgerDetailsScreen
                                   Navigator.push(
